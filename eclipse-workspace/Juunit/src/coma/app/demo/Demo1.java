e4586767676196b621401647701a08c7deaa778f
SPrash12 <prashantangularminds.in>
2021-10-27 12:13:46 +0530

    juunit added

---
+++ eclipse-workspace/Juunit/src/coma/app/demo/Demo1.java
@@ -0,0 +1,17 @@
+package coma.app.demo;
+
+import org.junit.Test;
+
+public class Demo1 {
+	
+	@Test
+	public  void JunitMethod() {
+		System.out.println("junit test case demo..!");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub	
+       Demo1 d1= new Demo1();
+       d1.JunitMethod();
+	}
+
+}
